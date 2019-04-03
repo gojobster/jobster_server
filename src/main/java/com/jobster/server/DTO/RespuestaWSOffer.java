08e9d04ae59d8f9d5125855af88e750037943c0a
Andreu Marí <amari@cimne.upc.edu>
2019-04-03 23:24:25 +0200

    Página de search

--- src/main/java/com/jobster/server/DTO/RespuestaWSOffer.java
+++ src/main/java/com/jobster/server/DTO/RespuestaWSOffer.java
@@ -3,6 +3,9 @@ package com.jobster.server.DTO;
 import org.json.JSONObject;
 
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
 
 public class RespuestaWSOffer {
 	public int id_offer;
@@ -14,11 +17,13 @@ public class RespuestaWSOffer {
 	public int reward;
 	public Integer salary_min;
 	public Integer salary_max;
-	public Timestamp date_init;
+	public Timestamp date_start;
 	public Timestamp date_end;
+	public Integer time_from_start;
+	public Integer time_to_end;
 
 	public RespuestaWSOffer(int id_offer, String nameCompany, String path_image_company, String position, String summary, String city,
-							int reward, Integer salary_min, Integer salary_max, Timestamp date_init, Timestamp date_end) {
+							int reward, Integer salary_min, Integer salary_max, Timestamp date_start, Timestamp date_end) {
 		this.id_offer = id_offer;
 		this.nameCompany = nameCompany;
 		this.path_image_company = path_image_company;
@@ -28,8 +33,10 @@ public class RespuestaWSOffer {
 		this.reward = reward;
 		this.salary_min = salary_min;
 		this.salary_max = salary_max;
-		this.date_init = date_init;
 		this.date_end = date_end;
+		this.date_start = date_start;
+		this.time_from_start = daysFromNow(date_start);
+		this.time_to_end = daysFromNow(date_end);
 	}
 
 	public RespuestaWSOffer(JSONObject object){
@@ -43,10 +50,20 @@ public class RespuestaWSOffer {
 			this.reward = object.getInt("reward");
 			this.salary_min = object.getInt("salary_min");
 			this.salary_max = object.getInt("salary_max");
-			this.date_init = new Timestamp(Long.parseLong(object.getString("date_init")));
+			this.date_start = new Timestamp(Long.parseLong(object.getString("date_start")));
 			this.date_end = new Timestamp(Long.parseLong(object.getString("date_end")));
+			this.time_from_start = daysFromNow(date_start);
+			this.time_to_end = daysFromNow(date_end);
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+	private Integer daysFromNow(Timestamp myDate){
+		LocalDate now = LocalDate.now();
+		LocalDate theDate = myDate.toLocalDateTime().toLocalDate();
+		Period period = Period.between(theDate, now);
+		return Math.abs(period.getDays());
+	}
+
 }
